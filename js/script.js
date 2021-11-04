abd2b0b58b5078fc7f685d5fd977b524b23eb74b
Ale-97 <oissela999@gmail.com>
2021-11-04 16:57:08 +0100

    1.1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
+//DICHIARAZIONE VARIABILI
 var checkClick = document.getElementsByClassName("check-box");
+var outOldPoints =document.getElementById('old-point');
+var outPoints = document.getElementById('points');
+var outNumberMine = document.getElementById('mine');
 var mine =[];
-var t ;
-for(var x = 1;x <= 16;x++){
+var difficulty=prompt('Con quale difficoltà vuoi giocare:Facile,Media,Difficile?')
+var numberMine;
+var points=0;
+var oldPoints=0;
+//INIZIO
+if (isNaN(sessionStorage.getItem("oldPoints")) || sessionStorage.getItem("oldPoints") == null){
+    outOldPoints.innerHTML += oldPoints;
+}else{   
+    oldPoints = sessionStorage.getItem("oldPoints");
+    outOldPoints.innerHTML += oldPoints;
+}
+
+outPoints.innerHTML += points;
+
+// CREO LE MINE IN BASE ALLA DIFFICOLTA SCELTA DALL'UTTENTE.
+switch(difficulty){
+    case "Facile" :
+        numberMine=16;
+        break;
+    case "Media" :
+        numberMine=32;
+        break;
+    case "Difficile" :
+        numberMine=64;
+        break;
+    default :  
+        numberMine=16;  
+}
+outNumberMine.innerHTML += numberMine;
+//CREO LE MINE E CONTROLLO CHE NON SE NE CREINO DOPPIONI;
+for(var x = 1;x <= numberMine;x++){
     var mina = (Math.floor(Math.random() * 100) + 1);
     for(var y = 0; y <= mine.length; y++){
         if(mina === mine[y]){ 
@@ -11,24 +44,47 @@ for(var x = 1;x <= 16;x++){
     } 
     mine.push(mina);
 }
-for(t = 0;t < checkClick.length; t++){
+//DO IL VALORE A TUTTI I BOX(IN QUESTO CASO A 1 A 100)COSI DA NON DOVERLI DARE IO SINGOLARMENTE
+for(var t = 0;t < checkClick.length; t++){
     var h = t + 1;
     checkClick[t].value = h;
 }
-console.log(mine);
-
-
+//CREO UN CICLO COSì DA PRENDERE IN CONSIDERAZIONE TUTTI I BOX
 for(var z = 0;z <= checkClick.length; z++){
 
-    checkClick[z].addEventListener('click', 
-    function(){
-            if(mine.includes(parseInt(this.value))){
-                this.className = this.classList + " color-red";
-                alert('HAI PERSO!')
-            }else{
-                this.className = this.classList + " color";
-            }       
-    })
+    checkClick[z].onclick = function(){
+
+        //QUI SCRIVO COSA VERRà ESEGUITO AL CLICK
+        var w = parseInt(this.value);
+        var area = [w-11,w-10,w-9,w-1,w+1,w+9,w+10,w+11];
+        var distance = 0;
+    
+        if(mine.includes(w)){
+            this.className = "color-red";
+            alert('HAI PERSO!');
+            
+        }
+        else{
+            for(var p = 0; p < 8;p++){
+                if( mine.includes(area[p])){
+                distance = distance + 1
+                }
+            }
+            if(distance<1){
+                this.className ="color";
+                points = points + 1;
+               
+    
+            }else if(distance > 0 && distance < 8) {
+                this.value=distance;
+                this.className = "color-distance";
+                points = points + 1;
+            }
+        } 
+        outPoints.innerHTML =  points;
+        sessionStorage.setItem("oldPoints", points);
+    
+    };
 }
 
 
