f31d5472cc2d0434ab086caac904f3dc23019a77
Ale-97 <oissela999@gmail.com>
2021-11-08 23:16:07 +0100

    1.3

--- js/script.js
+++ js/script.js
@@ -1,106 +1,111 @@
 //DICHIARAZIONE VARIABILI
 var checkClick = document.getElementsByClassName("check-box");
-var outOldPoints =document.getElementById('old-point');
+var outOldPoints = document.getElementById('old-point');
 var outPoints = document.getElementById('points');
-var outNumberMine = document.getElementById('mine');
-var mine =[];
-var difficulty=prompt('Con quale difficoltà vuoi giocare:Facile,Media,Difficile?')
-var numberMine;
-var points=0;
-var oldPoints=0;
-//INIZIO
-if (isNaN(sessionStorage.getItem("oldPoints")) || sessionStorage.getItem("oldPoints") == null){
+var outNumberMines = document.getElementById('mines');
+var mines = [];
+var numberMines;
+var points = 0;
+var oldPoints = 0;
+
+//iNIZIO CON IL CONTROLLARE SE IL GIOCATORE HA FATTO UNA PARTITA IN QUESTA SESSIONE, SE "SI" CARICO IL VECCHIO PUNTEGGIO, IN CASO CONTRARIO STAMPO 0
+if (isNaN(sessionStorage.getItem("oldPoints")) || sessionStorage.getItem("oldPoints") == null) {
     outOldPoints.innerHTML += oldPoints;
-}else{   
+} else {
     oldPoints = sessionStorage.getItem("oldPoints");
     outOldPoints.innerHTML += oldPoints;
 }
-
 outPoints.innerHTML += points;
 
-// CREO LE MINE IN BASE ALLA DIFFICOLTA SCELTA DALL'UTTENTE.
-switch(difficulty){
-    case "Facile" :
-        numberMine=16;
-        break;
-    case "Media" :
-        numberMine=32;
-        break;
-    case "Difficile" :
-        numberMine=64;
-        break;
-    default :  
-        numberMine=16;  
+// CHIEDO ALL'UTTENTE CON QUALE DIFFICOLTA VUOLE GIOCARE, POI A SECONDA DELLA DIFFICOLTà LE MINE AUMENTERANNO.
+while (difficulty !== "FACILE" && difficulty !== "MEDIA" && difficulty !== "DIFFICILE" && difficulty !== "") {
+    var difficulty = prompt('Con quale difficoltà vuoi giocare:Facile,Media,Difficile?').toUpperCase();
 }
-outNumberMine.innerHTML += numberMine;
-//CREO LE MINE E CONTROLLO CHE NON SE NE CREINO DOPPIONI;
-for(var x = 1;x <= numberMine;x++){
-    var mina = (Math.floor(Math.random() * 100) + 1);
-    for(var y = 0; y <= mine.length; y++){
-        if(mina === mine[y]){ 
-            mina = (Math.floor(Math.random() * 100) + 1);
-            y = 0;
-        }
-    } 
-    mine.push(mina);
+switch (difficulty) {
+    case "FACILE":
+        numberMines = 16;
+        break;
+    case "MEDIA":
+        numberMines = 32;
+        break;
+    case "DIFFICILE":
+        numberMines = 64;
+        break;
+    default:
+        numberMines = 16;
 }
-//DO IL VALORE A TUTTI I BOX(IN QUESTO CASO A 1 A 100)COSI DA NON DOVERLI DARE IO SINGOLARMENTE
-for(var t = 0;t < checkClick.length; t++){
-    var h = t + 1;
-    checkClick[t].value = h;
+outNumberMines.innerHTML += numberMines;
+
+//QUI SI GENERANO LE MINE, OVVERO DEI NUMERI CHE CORRISPONDERANNO ALLE COORDINATE DELLE MINE;
+while (numberMines !== mines.length) {
+    do {
+        var mine = (Math.floor(Math.random() * 100) + 1);
+
+    } while (mines.includes(mine))
+
+    mines.push(mine);
 }
-//CREO UN CICLO COSì DA PRENDERE IN CONSIDERAZIONE TUTTI I BOX
-for(var z = 0;z < checkClick.length; z++){
 
-    checkClick[z].onclick = function(){
+//DO IL VALORE A TUTTI I BOX(IN QUESTO CASO A 1 A 100) COSì DA NON DOVERLI DARE IO SINGOLARMENTE A TUTTI GLI ELEMENTI NELL'HTML
+for (var t = 0; t < checkClick.length; t++) {
+    checkClick[t].value = t + 1;
+}
 
-        //QUI SCRIVO COSA VERRà ESEGUITO AL CLICK
-        var w = parseInt(this.value);
-        var area = [w-11,w-10,w-9,w-1,w+1,w+9,w+10,w+11];
-        var areaLeft=[10,20,30,40,50,60,70,80,90,100];
-        var areaRight=[1,11,21,31,41,51,61,71,81,91];
+//CREO UN CICLO COSì DA DARE LA FUNZIONE A TUTTI I BOX
+for (var z = 0; z < checkClick.length; z++) {
 
+    checkClick[z].onclick = function () {
 
-        var distance = 0;
-    
-        if(mine.includes(w)){
-
-            for(var r = 0;r < checkClick.length; r++){
-                if(mine.includes(parseInt(checkClick[r].value))){
-                    checkClick[r].parentNode.classList.add("color-red");
+        //QUI SCRIVO COSA VERRà ESEGUITO AL CLICK NEL BOX
+        var valueBox = parseInt(this.value);
+        var perimeterControl = [valueBox - 11, valueBox - 10, valueBox - 9, valueBox - 1, valueBox + 1, valueBox + 9, valueBox + 10, valueBox + 11];
+        var allFirstBoxs = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+        var allLastBoxs = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91];
+        var distanceMines = 0;
+        //CONTROLLO CHE IL VALORE DEL BOX APPENA CLICCATO NON SIA COMPRESO NELLE MINE
+        if (mines.includes(valueBox)) {
+            //NEL CASO VENGA PRESA UNA MINA TUTTI I RIQUADRI DIVENTANO "DISPLAY:NONE" E COMPAIONO TUTTE LE MINE
+            for (var r = 0; r < checkClick.length; r++) {
+                if (mines.includes(parseInt(checkClick[r].value))) {
+                    checkClick[r].parentNode.classList.add("mine");
                 }
                 checkClick[r].style.display = "none";
             }
             alert('HAI PERSO!!!');
         }
-        else{
-            for(var p = 0; p < area.length;p++){
-                if( mine.includes(area[p])){
-                    if((areaLeft.includes(area[p]) && areaRight.includes(w)) || (areaRight.includes(area[p]) && areaLeft.includes(w))){}else{
-                        distance = distance + 1; 
+        else {
+            //SE NON è STATA PRESA UNA MINA, CONTROLLO SE ATTORNO AL MIO BOX CI SONO DELLE MINE
+            this.style.display = "none";
+
+            for (var p = 0; p < perimeterControl.length; p++) {
+                if (mines.includes(perimeterControl[p])) {
+                    if (!((allFirstBoxs.includes(perimeterControl[p]) && allLastBoxs.includes(valueBox)) || (allLastBoxs.includes(perimeterControl[p]) && allFirstBoxs.includes(valueBox)))) {
+                        distanceMines = distanceMines + 1;
                     }
                 }
             }
-            
-            if(distance<1){
-                this.parentNode.classList.add("color");
-                this.style.display = "none";
+            //SE NON CI SONO MINE ATTORNO SI LIMITERà A STAMPARE UN FIORE ED AD AGGIUNGERE UN PUNTO
+            if (distanceMines < 1) {
+                this.parentNode.classList.add("flower");
                 points = points + 1;
-               
-    
-            }else if(distance > 0 && distance < 8) {
-                this.parentNode.classList.add("color");
-                this.parentNode.innerHTML = distance;
+                console.log(distanceMines);
+            }
+            // SE CI DOVESSERO ESSERE DELLE MINE ATTORNO, STAMPERà IL NUMERO DI MINE VICINE ED AGGIUNGERà UN PUNTO
+            else {
+                this.parentNode.classList.add("radar");
+                this.parentNode.innerHTML = distanceMines;
                 points = points + 1;
             }
-        } 
-
-        outPoints.innerHTML =  points;
-        if(points === (100-numberMine)){
+        }
+        //STAMPA IL PUNTEGGIO
+        outPoints.innerHTML = points;
+        //SE VENGONO TROVATE TUTTE LE CASELLE SENZA MINE COMPARIRà LA SCRITTA DI VITTORIA.
+        if (points === (100 - numberMines)) {
             alert('HAI VINTO!');
         }
+        //VIENE SALVATO IL PUNTEGGIO COSI CHE AL REFRESH DELLA PAGINA SI POSSA VEDERE IL VECCHIO PUNTEGGIO.
         sessionStorage.setItem("oldPoints", points);
-    
+
     };
 }
 
